cdf0673e59b56df540b39c8d7439a091dc9fdd40
Michael S. P. Kelley <msk@astro.umd.edu>
2024-07-08 15:04:13 -0400

    Expand comments around search vs fetch_field in examples.

--- examples/lco-skymapper-constant.py
+++ examples/lco-skymapper-constant.py
@@ -9,45 +9,53 @@ from astropy.io import fits
 from astropy.coordinates import SkyCoord
 import calviacat as cvc
 
-if os.path.exists('lco.fits'):
-    hdu = fits.open('lco.fits')
+if os.path.exists("lco.fits"):
+    hdu = fits.open("lco.fits")
 else:
-    r = requests.get('https://archive-api.lco.global/frames/6913550/').json()
-    hdu = fits.open(r['url'])
-    hdu.writeto('lco.fits')
+    r = requests.get("https://archive-api.lco.global/frames/6913550/").json()
+    hdu = fits.open(r["url"])
+    hdu.writeto("lco.fits")
 
-im = hdu['sci'].data
-h = hdu['sci'].header
-phot = Table(hdu['cat'].data)
+im = hdu["sci"].data
+h = hdu["sci"].header
+phot = Table(hdu["cat"].data)
 
-phot = phot[phot['FLAG'] == 0]  # clean LCO catalog
-lco = SkyCoord(phot['RA'], phot['DEC'], unit='deg')
+phot = phot[phot["FLAG"] == 0]  # clean LCO catalog
+lco = SkyCoord(phot["RA"], phot["DEC"], unit="deg")
 
 # initialize catalog
-skym = cvc.SkyMapper('cat.db')
+skym = cvc.SkyMapper("cat.db")
 
-# download catalog?
-if len(skym.search(lco)[0]) < 500:
+# check the local catalog database for stars in the vicinity of the LCO catalog
+catalog_ids, coordinates = skym.search(lco)
+
+# If there are no objects in the local database, then we need to retrieve them
+# from PS1's online database.  For these LCO data, finding anything fewer than
+# 500 catalog objects suggests there may only be partial overlap between the
+# local database and the LCO data.  This number will depend on image depth and
+# location on the sky.
+if len(catalog_ids) < 500:
+    # search the PS1 online database for objects in the vicinity of the LCO
+    # catalog
     skym.fetch_field(lco)
 
 # crossmatch LCO photometry table with catalog
 objids, distances = skym.xmatch(lco)
 
 # Calibrate this g-band image
-g_inst = -2.5 * np.log10(phot['FLUX'])
-g_err = phot['FLUXERR'] / phot['FLUX'] * 1.0857
+g_inst = -2.5 * np.log10(phot["FLUX"])
+g_err = phot["FLUXERR"] / phot["FLUX"] * 1.0857
 
 # no color info, so make g-i unlimited
-zp_mean, zp_median, unc, g, gmi = skym.cal_constant(
-    objids, g_inst, 'g', gmi_lim=None)
+zp_mean, zp_median, unc, g, gmi = skym.cal_constant(objids, g_inst, "g", gmi_lim=None)
 
 # plot results
 fig = plt.figure(1)
 fig.clear()
 ax = fig.gca()
-ax.scatter(g, g - g_inst, marker='.', color='k')
+ax.scatter(g, g - g_inst, marker=".", color="k")
 ax.axhline(zp_mean)
 ax.axhline(zp_median)
-plt.setp(ax, xlabel='$g$ (mag)', ylabel=r'$g - g_{\rm inst}$ (mag)')
+plt.setp(ax, xlabel="$g$ (mag)", ylabel=r"$g - g_{\rm inst}$ (mag)")
 plt.tight_layout()
-plt.savefig('lco-skymapper-constant.png', dpi=150)
+plt.savefig("lco-skymapper-constant.png", dpi=150)
